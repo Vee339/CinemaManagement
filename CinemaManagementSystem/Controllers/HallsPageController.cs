e297d5797306569ea710c3cc133dc50bca84c518
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-04-19 15:36:50 -0400

    Build the Web Pages for Hall

--- CinemaManagementSystem/Controllers/HallsPageController.cs
+++ CinemaManagementSystem/Controllers/HallsPageController.cs
@@ -2,6 +2,7 @@
 using CinemaManagementSystem.Models;
 using CinemaManagementSystem.Interfaces;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using CinemaManagementSystem.Models.ViewsModels;
 
 namespace CinemaManagementSystem.Controllers
 {
@@ -21,11 +22,75 @@ namespace CinemaManagementSystem.Controllers
         }
 
         // Get: HallsPage/List
+        [HttpGet]
         public async Task<IActionResult> List()
         {
             IEnumerable<Hall> Halls = await _hallsService.GetHalls();
 
             return View(Halls);
+        }
+
+        // Get: /HallsPage/Details/{id}
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            Hall Hall = await _hallsService.FindHall(id);
+            IEnumerable<ScreeningDto> Screenings = await _screeningsService.ListScreeningsForHall(id);
+
+            HallDetails HallInfo = new HallDetails()
+            {
+                Hall = Hall,
+                HallScreenings = Screenings
+            };
+
+            return View(HallInfo);
+        }
+
+        // Get: /HallsPage/New
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        // Post: /HallsPage/Add
+        [HttpPost]
+        public async Task<IActionResult> Add(Hall hall)
+        {
+            await _hallsService.AddHall(hall);
+            return RedirectToAction("List", "HallsPage");
+        }
+
+        // Get: /HallsPage/Edit
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Hall hall = await _hallsService.FindHall(id);
+            return View(hall);
+        }
+
+        // POST: /HallsPage/Update/{id}
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Hall hall)
+        {
+            await _hallsService.EditHall(id, hall);
+            return RedirectToAction("Details", "HallsPage", new { id= id });
+        }
+
+        // Get: /HallsPage/ConfirmDelete/{id}
+        [HttpGet]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            Hall hall = await _hallsService.FindHall(id);
+            return View(hall);
+        }
+
+        // Post: /HallsPage/Delete/{id}
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _hallsService.DeleteHall(id);
+            return RedirectToAction("List", "HallsPage");
         }
     }
 }
---
+++ CinemaManagementSystem/Models/ViewsModels/HallDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaManagementSystem.Models.ViewsModels
+{
+    public class HallDetails
+    {
+        // A hall page must have the information about the hall
+        public required Hall Hall { get; set; }
+
+        public IEnumerable<ScreeningDto>? HallScreenings { get; set; }
+    }
+}
